92c3b2843d29db8c05d939b7f5c32a544ea82543
Levy <kipkoechlevy096@gmail.com>
2023-02-09 15:25:42 +0300

    completed assignment

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,19 @@
 class Dog
+     attr_reader :name
+     attr_accessor :breed
+    def name=(name)
+        @name = name
+    end
+
+    def breed=(breed)
+        @breed = breed
+    end
 
 end
+
+Fido = Dog.new
+Fido.name = 'Fido' 
+
+snoopy = Dog.new 
+
+snoopy.breed = 'Beagle' 
